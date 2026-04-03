f11001f3b722a22d121df8e830c1a122a0d218ee
Gene Luevano <geneluevano@gmail.com>
2026-04-03 03:21:43 -0700

    adding comments to ForceModel

--- createForceModel.py
+++ createForceModel.py
@@ -3,25 +3,52 @@ from load_gmat import *
 
 
 class ForceModel:
+    """This class is a wrapper for a ForceModel object in GMAT.
+
+    Provided that this force model is supposed to support the reference satellite object
+    or the truth satellite, this class will assign the corresponding forces and perturbations
+
+    Inputs during intialization:
+        fmType: MUST BE EITHER "reference" or "truth" (non-case specific, determines the forces that need to be modeled)
+
+    - If a reference force model, only perturbations considered are a 2x0 Earth geopotential model
+    - If a truth force model, the following perturbations are considered:
+        - 4x4 Earth geopotential model
+        - Atmospheric drag
+        - 3rd body effects (Lunar and Solar)
+        - Solar radiation pressure
+
+    Variables:
+        fm: GMAT ForceModel Object (object containing all described forces)
+        burn: GMAT FiniteBurn Object (object describing finite thrust along a corresponding RIC axis)
+        burnforce: GMAT Force to be applied to a finite burn object (what gets added to the force model for propagation)
+    """
+
     def __init__(self, fmType: str):
+        """Initialize the ForceModel wrapper with ForceModel type."""
         self.fm = gmat.Construct("ForceModel", f"{fmType}_Forces")
         self.burn = {}
         self.burnForce = {}
 
     def getBurnForce(self, axis):
+        """Return the GMAT BurnForce object"""
         return self.burnForce[axis]
 
     def getFM(self):
+        """Return the GMAT ForceModel object"""
         return self.fm
 
     def setDynamics(self, propType: str):
+        """Assign the corresponding forces to the ForceModel"""
+        
+        # ForceModel type must be either "reference" or "truth" otherwise an error is rasied
         if propType.lower() != "reference" and propType.lower() != "truth":
             raise ValueError(f"Incorrect propagation type was chosen (Provided: {propType}). The propagator type must only be 'reference' or 'truth'.")
         
         if propType.lower() == "reference":
             self._setForces(
                 degree=2,
-                order=2
+                order=0
             )
         else:
             self._setForces(
